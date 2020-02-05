17dc5f55276648011ddc7b96901ff238244e8653
= <=>
2020-02-05 23:32:10 +0100

    Added navigation menu

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React from "react";
 import "./App.css";
 import { Home } from "./views/Home";
 import { PhotoCropper } from "./views/PhotoCropper";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { AppBar, Toolbar, Tab, Tabs } from "@material-ui/core";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/PhotoCropper" exact component={PhotoCropper}></Route>
-        <Route path="/" render={() => <div>404</div>}></Route>
-      </Switch>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <AppBar position="static">
+          <Toolbar>
+            <Tabs>
+              <Tab label="Home" to="/" component={Link} />
+              <Tab label="PhotoCropper" to="/PhotoCropper" component={Link} />
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/PhotoCropper" exact component={PhotoCropper}></Route>
+          <Route path="/" render={() => <div>404</div>}></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
