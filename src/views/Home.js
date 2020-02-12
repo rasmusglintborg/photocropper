abeac9e7680318a7963a6ee84d7ca457f968462e
= <=>
2020-02-12 22:13:08 +0100

    Added text for homepage

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,26 +1,48 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(theme => ({
+  header: {
+    padding: "60px",
+    overflow: "hidden"
+  }
+}));
+
 export const Home = () => {
+  const classes = useStyles();
+
   return (
-    <Grid style={{ marginTop: "5px", marginLeft: "5px", marginRight: "5px" }}>
+    <Grid style={{ marginTop: "2px", marginLeft: "5px", marginRight: "5px" }}>
       <Grid>
-        <div>
-          <h3>This is a quick and dirty image cropper</h3>
+        <div className={classes.header}>
+          <Typography align="center" variant="h2" color="primary" gutterBottom>
+            image cropper
+          </Typography>
+          <Typography align="center" variant="h4" gutterBottom>
+            This is a quick and dirty image cropper
+          </Typography>
         </div>
       </Grid>
       <Grid>
         <div>
-          <p>
-            This is a quick and dirty image cropper for the digital photoframe
-            from Denver. The model of the photoframe is&nbsp;
+          <Typography align="center" variant="subtitle1" gutterBottom>
+            This image cropper is made for the digital photoframe from
+            Denver-eletronics. The model of the photoframe is&nbsp;
             <a href="https://denver-electronics.com/products/frameo/denver-pff-1010black/c-1024/c-1262/p-2991">
               PFF-1010BLACK MK2
             </a>
-          </p>
-          <h3>
-            source code can be found on &nbsp;
+            &nbsp;with the app Denver-eletronic made for cropping images,{" "}
+            <br></br>
+            you cant display the timestamp for when the photo was taken which is
+            important context for some photos.
+          </Typography>
+
+          <Typography align="center" variant="h4" gutterBottom>
+            source code can be found on&nbsp;
             <a href="https://github.com/rasmusglintborg/photocropper">github</a>
-          </h3>
+          </Typography>
         </div>
       </Grid>
     </Grid>
